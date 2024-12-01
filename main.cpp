9fad03fe5026959ed57e2c905b89541441a85b66
Mostafa <mostafa.dream9911@gmail.com>
2024-12-01 18:20:37 +0200

    add find book method in main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include "List.h"  
 using namespace std;
-int main()
-{
+
+void FindBook(List lib) {
+    string title;
+    cout << "Enter the title of the book you search: ";
+    cin >> title;
+
+    Book* book = lib.find(title);
+
+    if( book != nullptr){
+        cout << "Book found \n";
+        // display book details
+
+    }
+    else{
+        cout << "book not found \n";
+    }
+}
+
+
+
+int main() {
+    // FindBook();
     return 0;
 }
