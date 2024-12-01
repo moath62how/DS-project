a5de0fdd8d3ed57c223f6869b552cb5c7224c305
Nader2005 <Nader2005@example.com>
2024-12-01 19:12:25 +0200

    I'm formatting the code

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@ void findBook(List &lib)
 {
     string title;
     cout << "Enter the title of the book you search: ";
-    cin.ignore();  
-    getline(cin, title); 
+    cin.ignore();
+    getline(cin, title);
 
     Book *book = lib.find(title);
 
@@ -22,27 +22,32 @@ void findBook(List &lib)
     }
 }
 
-void BorrowBook(List &lib){
-    cout <<"Enter the title of the book that you want to borrow: ";
+void BorrowBook(List &lib)
+{
+    cout << "Enter the title of the book that you want to borrow: ";
     string title;
-    cin.ignore();  
-    getline(cin, title); 
+    cin.ignore();
+    getline(cin, title);
 
-    Book*book = lib.find(title);
+    Book *book = lib.find(title);
 
-    if(book != nullptr){
-        if(book->copies>0){
+    if (book != nullptr)
+    {
+        if (book->copies > 0)
+        {
             book->copies--;
-            cout <<"you have borrowed the boook: "<<book->title<<endl;
-            cout << "Remaining copies: "<<book->copies<<endl;
+            cout << "you have borrowed the boook: " << book->title << endl;
+            cout << "Remaining copies: " << book->copies << endl;
         }
-        else{
-            cout <<"Sorry, no copies available to borrow.\n";
+        else
+        {
+            cout << "Sorry, no copies available to borrow.\n";
         }
     }
-    else{
+    else
+    {
         cout << "book not found \n";
-        }
+    }
 }
 
 int main()
