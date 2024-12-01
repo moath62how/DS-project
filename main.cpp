048f0319e8e6b89c58d9ab0b4b09783a978d46a8
Mostafa <mostafa.dream9911@gmail.com>
2024-12-01 18:55:13 +0200

    borrow book

--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@ using namespace std;
 void FindBook(List lib) {
     string title;
     cout << "Enter the title of the book you search: ";
-    cin >> title;
+    cin.ignore();  
+    getline(cin, title); 
 
     Book* book = lib.find(title);
 
@@ -17,6 +18,29 @@ void FindBook(List lib) {
     else{
         cout << "book not found \n";
     }
+}
+
+void BorrowBook(List lib){
+    cout <<"Enter the title of the book that you want to borrow: ";
+    string title;
+    cin.ignore();  
+    getline(cin, title); 
+
+    Book*book = lib.find(title);
+
+    if(book != nullptr){
+        if(book->copies>0){
+            book->copies--;
+            cout <<"you have borrowed the boook: "<<book->title<<endl;
+            cout << "Remaining copies: "<<book->copies<<endl;
+        }
+        else{
+            cout <<"Sorry, no copies available to borrow.\n";
+        }
+    }
+    else{
+        cout << "book not found \n";
+        }
 }
 
 
